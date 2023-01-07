54ac348a05777ad38a2ccd355a9b672db3b02ad8
pablo marino <pablo-n-m@hotmail.com>
2023-01-06 23:00:49 -0500

    en baseline ready

--- evaluator.py
+++ evaluator.py
@@ -1,9 +1,21 @@
 import argparse
 from deepspeechWrapper import ASR as DPASR
+from jiwer import wer
+import numpy as np
 
 #TODO: add my recorded audio
 AUDIO_FILES = {
-    "EN": ["parents.wav", "checkin.wav", "what_time.wav", "suitcase.wav", "where.wav"]
+    "EN": ["parents.wav", "checkin.wav", "what_time.wav", "suitcase.wav", "where.wav", "your_sentence1.wav", "your_sentence2.wav"]
+}
+
+GROUND_TRUTHS = {
+    "parents.wav": "I have lost my parents",
+    "checkin.wav": "Where is the check-in desk?",
+    "what_time.wav": "What time is my plane?",
+    "suitcase.wav": "Please, I have lost my suitcase",
+    "where.wav":"Where are the restaurants and shops?",
+    "your_sentence1.wav": "I want the reimbursement of my flight",
+    "your_sentence2.wav": "Have you seen my dog?"
 }
 
 
@@ -13,10 +25,14 @@ def run(system, lang):
         acr = DPASR(lang)
     SOUNDS_DIR = "sounds/"+lang
     audio_filenames = AUDIO_FILES[lang]
+    WERs = []
     for audio_filename in audio_filenames:
         prediction = acr.predict(f"{SOUNDS_DIR}/{audio_filename}")
-        print(f"prediction for {audio_filename}: {prediction}")
-
+        ground_truth = GROUND_TRUTHS[audio_filename]
+        error_rate = wer(ground_truth.lower(), prediction.lower())
+        WERs.append(error_rate)
+        print(f'prediction for {audio_filename}: "{prediction}" - ground_truth:"{ground_truth}" -wer: {error_rate}')
+    print(f"avg WER for system: {system} and lang: {lang}: ", np.mean(WERs))
 
 
 
